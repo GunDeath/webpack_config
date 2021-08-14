71b60b5641ebefccb0f1aaac9b75713bd76c6b52
Gun_Death <phillbleck@gmail.com>
2021-08-14 22:45:06 +0300

    add babel config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,20 @@
 const path = require('path')
 
 module.exports = {
-
+    mode: 'development',
+    //devtool: false,
+    devServer: {
+        contentBase: './dist',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                }
+            }
+        ]
+    }
 }
